f75cb0adb60429f7d9fdac47ee06cd9db12b4acb
khg <120825235+potone1316@users.noreply.github.com>
2022-12-20 10:33:27 +0900

    멤버중간업

--- petCenter/src/main/pc/member/controller/MemberController.java
+++ petCenter/src/main/pc/member/controller/MemberController.java
@@ -9,24 +9,32 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import main.pc.member.service.MemberService;
+
 
 @Controller
 public class MemberController {
 	Logger logger = LogManager.getLogger(MemberController.class);
-
-
-	// 입력
-	@RequestMapping(value="kmjSpringMemInsertForm", method=RequestMethod.GET)
 	
-	public String kmjSpringMemInsertForm() {
-		logger.info("kmjSpringMemInsertForm 함수 진입 : ");
-		
-		return "mem/kmjSpringMemInsertForm";
+	// 컨트롤러에서 서비스 필드 오토 와이어드
+	@Autowired(required=false)
+	private MemberService memberService;
+	
+	// 컨트롤러에서 채번 서비스 필드 오토 와이어드
+//	@Autowired(required=false)
+//	private ChabunService chabunService;
+	
+	//인서트 폼 호출하기
+	@GetMapping("memInsertForm")
+	public String memInsertForm() {
+		logger.info("memInsertForm 함수 진입 : ");
+		return "mem/memInsertForm";
 	}
 	
 }
