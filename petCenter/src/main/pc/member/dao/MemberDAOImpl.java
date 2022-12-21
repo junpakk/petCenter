f277854e3062b556043a71e15f5be76b627c282f
potone1316 <120825235+potone1316@users.noreply.github.com>
2022-12-21 16:27:57 +0900

    누락2

---
+++ petCenter/src/main/pc/member/dao/MemberDAOImpl.java
@@ -0,0 +1,62 @@
+package main.pc.member.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import main.pc.member.vo.MemberVO;
+
+@Repository
+public class MemberDAOImpl implements MemberDAO {
+	Logger logger = LogManager.getLogger(getClass());
+	
+	@Autowired(required=false)
+	private SqlSession sqlSession;
+	
+	@Override
+	public int memInsert(MemberVO mvo) {
+		// TODO Auto-generated method stub
+		logger.info("memInsert() 함수 진입 >>> : ");
+		return sqlSession.insert("memInsert", mvo);
+	}
+
+	@Override
+	public List<MemberVO> memSelectAll(MemberVO mvo) {
+		// TODO Auto-generated method stub
+		logger.info("memSelectAll() 함수 진입 >>> : ");
+		return sqlSession.selectList("memSelectAll", mvo);
+	}
+
+	@Override
+	public List<MemberVO> memSelect(MemberVO mvo) {
+		// TODO Auto-generated method stub
+		logger.info("memSelect() 함수 진입 >>> : ");
+		return sqlSession.selectList("memSelect", mvo);
+	}
+
+	@Override
+	public List<MemberVO> memSelectForm(MemberVO mvo) {
+		// TODO Auto-generated method stub
+		logger.info("memSelectForm() 함수 진입 >>> : ");
+		return null;
+	}
+
+	@Override
+	public int memUpdate(MemberVO mvo) {
+		// TODO Auto-generated method stub
+		logger.info("memUpdate() 함수 진입 >>> : ");
+		return sqlSession.update("memUpdate", mvo);
+	}
+
+	@Override
+	public int memDelete(MemberVO mvo) {
+		// TODO Auto-generated method stub
+		logger.info("memDelete() 함수 진입 >>> : ");
+		return sqlSession.update("memDelete", mvo);
+	}
+
+}
---
+++ petCenter/src/main/pc/member/service/MemberServiceImpl.java
@@ -0,0 +1,64 @@
+package main.pc.member.service;
+
+import java.util.List;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import main.pc.member.dao.MemberDAO;
+import main.pc.member.vo.MemberVO;
+
+@Service
+@Transactional
+public class MemberServiceImpl implements MemberService {
+	Logger logger = LogManager.getLogger(getClass());
+	
+	@Autowired(required=false)
+	private MemberDAO memberDAO;
+	
+	@Override
+	public int memInsert(MemberVO mvo) {
+		// TODO Auto-generated method stub
+		logger.info("memInsert() 함수 진입 >>> : ");
+		return memberDAO.memInsert(mvo);
+	}
+
+	@Override
+	public List<MemberVO> memSelectAll(MemberVO mvo) {
+		// TODO Auto-generated method stub
+		logger.info("memSelectAll() 함수 진입 >>> : ");
+		return memberDAO.memSelectAll(mvo);
+	}
+
+	@Override
+	public List<MemberVO> memSelect(MemberVO mvo) {
+		// TODO Auto-generated method stub
+		logger.info("memSelect() 함수 진입 >>> : ");
+		return memberDAO.memSelect(mvo);
+	}
+
+	@Override
+	public List<MemberVO> memSelectForm(MemberVO mvo) {
+		// TODO Auto-generated method stub
+		logger.info("memSelectForm() 함수 진입 >>> : ");
+		return null;
+	}
+
+	@Override
+	public int memUpdate(MemberVO mvo) {
+		// TODO Auto-generated method stub
+		logger.info("memUpdate() 함수 진입 >>> : ");
+		return memberDAO.memUpdate(mvo);
+	}
+
+	@Override
+	public int memDelete(MemberVO mvo) {
+		// TODO Auto-generated method stub
+		logger.info("memDelete() 함수 진입 >>> : ");
+		return memberDAO.memDelete(mvo);
+	}
+
+}
